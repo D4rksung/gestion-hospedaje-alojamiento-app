cffd1cf8ec26682ec65173895c9bd81388337817
antonio_amado <ac.marcel@gmail.com>
2017-07-10 12:25:11 -0500

    Modificar rutas de api

--- src/app/core/config.ts
+++ src/app/core/config.ts
@@ -1,8 +1,10 @@
+const ROOT_PATH = 'http://petcentermockapi.azurewebsites.net';
+
 export let CONFIG = {
   baseUrls: {
     config: 'commands/config',
-    alimentos: 'http://localhost:3000/alimentos',
-    filtros: 'http://localhost:3000/filtros',
-    planesAlimenticios:'http://localhost:3000/planesAlimenticios'
+    alimentos: `${ROOT_PATH}/alimentos`,
+    filtros: `${ROOT_PATH}/filtros`,
+    planesAlimenticios: `${ROOT_PATH}/planesAlimenticios`
   }
 };
